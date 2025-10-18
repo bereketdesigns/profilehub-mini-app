55f9996318d064a0ef5552f887271d362b113475
bereketdesigns <bereketdesigns@gmail.com>
2025-10-18 10:02:41 +0000

    fix(config): Explicitly enable SSR output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// It's slightly better to import from the 'serverless' entry point
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This is the crucial line that was missing.
+  // It tells Astro to build a dynamic, server-rendered application.
+  output: "server",
+  
+  // This tells Astro how to package the server code for Vercel.
+  adapter: vercel()
+});
